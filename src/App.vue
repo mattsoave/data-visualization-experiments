90ac041404f84c78a1aa7077a45751c0b8f6fd97
Matt Soave <mattsoav@amazon.com>
2020-05-24 14:20:13 -0700

    Updated line chart to support multiple lines

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div style="height: 400px; background-color: #f7f7f7;">
-      <VueLineChart/>
+      <VueLineChart :vals="values"/>
     </div>
+    <button @click="addValue">Add</button>
   </div>
 </template>
 
@@ -13,6 +14,25 @@ export default {
   name: 'app',
   components: {
     VueLineChart
+  },
+  data() {
+    return {
+      values: [
+        [1, 3, 6, 8, 13],
+        [2, 5, 7, 10, 25],
+        [3, 4, 8, 10],
+      ]
+    };
+  },
+  methods: {
+    addValue() {
+      for (let j = 0; j < 30; j++) {
+        for (let i = 0; i < this.values.length; i++) {
+          this.values[i].push(this.values[i][this.values[i].length - 1]*(0.2*Math.random()+1));
+        }
+      }
+
+    },
   }
 }
 </script>
--- src/components/VueLineChart.vue
+++ src/components/VueLineChart.vue
@@ -1,24 +1,34 @@
 <template>
-  <div class="container" ref="container">
-    <svg class="svg" width="100%" height="100%">
-      <g>
-        <path :d="line" />
-      </g>
-      <g class="x-axis"></g>
-      <g class="y-axis"></g>
-    </svg>
-  </div>
+    <div class="container" ref="container">
+        <svg class="svg" ref='svg'>
+            <g>
+                <path v-for="(line, i) in lines" :d="line" :stroke='colors[i%10]' />
+            </g>
+            <g class="x-axis"></g>
+            <g class="y-axis"></g>
+        </svg>
+    </div>
 
 </template>
 
 <script>
   import * as d3 from 'd3';
+
+  window.d2 = d3;
+
   export default {
     name: 'VueLineChart',
     data() {
       return {
         data: [],
-        line: '',
+
+        svg: null,
+        x: d3.scaleLinear(),
+        y: d3.scaleLinear(),
+        lines: [],
+
+        colors: d3.schemeTableau10,
+
         geometry: {
           width: null,
           height: null
@@ -29,80 +39,108 @@
         }
       };
     },
-    created() {
-      for (let i = 0; i < 50; i++) {
-        this.data.push(Math.pow(i, 2 + Math.random()*.1) );
+    props: {
+      vals: {
+        type: Array,
+        default() {
+          return [];
+        },
       }
     },
+    created() {
+      for (let i = 0; i < 50; i++) {
+        this.data.push(Math.pow(i, 2 + Math.random() * .1));
+      }
+    },
+    watch: {
+      vals() {
+        this.refreshChart();
+      },
+    },
     mounted() {
+      this.svg = d3.select(this.$refs.svg);
       this.refreshChart();
       window.addEventListener('resize', this.refreshChart);
     },
     methods: {
       refreshChart() {
         // Update chart bounds
+        // Must get offsetWidth/height on container, not svg
         this.geometry.width = this.$refs.container.offsetWidth;
         this.geometry.height = this.$refs.container.offsetHeight;
 
         // Set the x-scale
-        this.d3Parameters.xScale
-                .range([60, this.geometry.width - 10])
-                .domain(d3.extent(this.data, (d, i) => i));
+        this.x
+          .range([60, this.geometry.width - 10])
+          .domain([this.processed.extremes.xMin, this.processed.extremes.xMax]);
         // Set the y-scale
-        this.d3Parameters.yScale
-                .range([this.geometry.height - 20, 10])
-                .domain([0, d3.max(this.data, d => d)]);
+        this.y
+          .range([this.geometry.height - 20, 10])
+          .domain([this.processed.extremes.yMin, this.processed.extremes.yMax]);
 
         // Assign the scales to the axes
-        d3.axisLeft().scale(this.d3Parameters.xScale);
-        d3.axisBottom().scale(this.d3Parameters.yScale);
+        d3.axisLeft().scale(this.x);
+        d3.axisBottom().scale(this.y);
 
         d3.select("g.x-axis")
-                .attr("transform", "translate(0," + (this.geometry.height - 20) + ")")
-                .call(d3.axisBottom(this.d3Parameters.xScale));
+          .attr("transform", "translate(0," + (this.geometry.height - 20) + ")")
+          .call(d3.axisBottom(this.x));
 
         d3.select("g.y-axis")
-                .attr("transform", "translate(60,0)")
-                .call(d3.axisLeft(this.d3Parameters.yScale));
+          .attr("transform", "translate(60,0)")
+          .call(d3.axisLeft(this.y));
 
 
         //
         const path = d3.line().curve(d3.curveNatural)
-                .x((d, i) => this.d3Parameters.xScale(i))
-                .y(d => this.d3Parameters.yScale(d))
-        this.line = path(this.data);
-      },
-      getScales() {
-        const x = d3.scaleTime().range([0, this.geometry.width]);
-        const y = d3.scaleLinear().range([this.geometry.height, 0]);
-        d3.axisLeft().scale(x);
-        d3.axisBottom().scale(y);
-        x.domain(d3.extent(this.data, (d, i) => i));
-        y.domain([0, d3.max(this.data, d => d)]);
-        console.log(x(4));
-        return { x, y };
-      },
-      calculatePath() {
-        const scale = this.getScales();
-        const path = d3.line()
-                .x((d, i) => scale.x(i))
-                .y(d => scale.y(d));
-        this.line = path(this.data);
+          .x((d, i) => this.x(i))
+          .y(d => this.y(d))
+        this.lines = this.processed.vals.map(i => path(i));
       },
     },
+    computed: {
+      processed() {
+        const processed = {};
+
+        if (Array.isArray(this.vals[0])) {
+          processed.vals = this.vals;
+        } else {
+          processed.vals = [this.vals];
+        }
+
+        const extremes = {
+          xMin: 0,
+          xMax: 0,
+          yMin: 0,
+          yMax: null
+        };
+
+        for (const valArr of processed.vals) {
+          if (valArr.length - 1 > extremes.xMax) extremes.xMax = valArr.length - 1;
+
+          const yMax = Math.max(...valArr);
+          if (yMax > extremes.yMax) extremes.yMax = yMax;
+        }
+        processed.extremes = extremes;
+
+        return processed;
+      }
+    }
   };
 </script>
 
 <style lang="scss" scoped>
-  .container, svg {
-    width: 100%;
-    height: 100%;
-  }
-  svg {}
-  path {
-    fill: none;
-    stroke: #76BF8A;
-    stroke-width: 3px;
-  }
+    .container, svg {
+        width: 100%;
+        height: 100%;
+    }
+
+    svg {
+    }
+
+    path {
+        fill: none;
+        stroke-width: 3px;
+    }
 
 </style>
