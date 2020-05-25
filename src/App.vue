d39cd1faa1500c7c16c02f538f336acb47f1c7c4
Matt Soave <mattsoav@amazon.com>
2020-05-25 00:11:54 -0700

    Added some filtering for logs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
       <div style="height: 600px; background-color: #ddd;">
+          <Chart2D :vals="values" :data="scatterData2" :options="{x:{type:'linear'}, y:{type:'log'}}"/>
+      </div>
+      <button @click="addValue">Add</button>
+      <div style="height: 600px; background-color: #ddd;">
           <VueScatter :vals="values" :data="scatterData"/>
       </div>
       <button @click="addValue">Add</button>
@@ -17,12 +21,14 @@
 <script>
 import VueLineChart from './components/VueLineChart.vue'
 import VueScatter from './components/VueScatter.vue'
+import Chart2D from './components/Chart2D.vue'
 
 export default {
   name: 'app',
   components: {
     VueLineChart,
-    VueScatter
+    VueScatter,
+    Chart2D
   },
   data() {
     return {
@@ -32,11 +38,15 @@ export default {
         [3, 4, 8, 10],
       ],
       scatterData: [
-        [[1, 1], [2, 2], [3, 4]],
-        [[0.1, 2], [0.2, 2.2], [0.3, 2.5]],
+        [[1, -1], [1.3, 0.3], [2, 2], [3, 4]],
+        [[-0.1, 2], [0.2, 2.2], [0.3, 2.5]],
         [[1.1, 2], [0.6, 3.2], [10.6, 4.5], [3, 6]],
-      ]
+      ],
+      scatterData2: [[]]
     };
+  },
+  created() {
+    this.scatterData2 = this.scatterData.map(a=>a);
   },
   methods: {
     addValue() {
@@ -47,10 +57,10 @@ export default {
         }
       }
       const newScatter = [];
-      for (let i = 0; i < 100; i++) {
-        newScatter.push([i, Math.sin(i/20)*20 + Math.random()*10 - 5 + 25]);
+      for (let i = 1; i < 10; i += 0.1) {
+        newScatter.push([i, Math.pow(2+ Math.random()/10, i ) + Math.random()*10]);
       }
-      this.scatterData.push(newScatter);
+      this.scatterData2.push(newScatter);
 
     },
   }
--- src/components/Chart2D.vue
+++ src/components/Chart2D.vue
@@ -16,13 +16,11 @@
   window.d2 = d3;
 
   export default {
-    name: 'VueScatter',
+    name: 'Chart2D',
     data() {
       return {
         svg: null,
         type: 'line',
-        x: d3.scaleLinear(),
-        // y: d3.scaleLog(),
         lines: [],
 
         colors: d3.schemeTableau10,
@@ -83,14 +81,15 @@
         this.x
           .range([60, this.geometry.width - 10])
           .domain([
-            this.extremes.xMin, this.extremes.xMax
+            this.opts.x.type === 'log' ? Math.max(this.extremes.xMin, this.extremes.xMinPositive) : this.extremes.xMin,
+            this.opts.x.type === 'log' ? Math.max(this.extremes.xMax, this.extremes.xMinPositive + 1) : this.extremes.xMax,
           ]);
         // Set the y-scale
         this.y
           .range([this.geometry.height - 20, 10])
           .domain([
-            this.extremes.yMin,
-            this.extremes.yMax
+            this.opts.y.type === 'log' ? Math.max(this.extremes.yMin, this.extremes.yMinPositive) : this.extremes.yMin,
+            this.opts.y.type === 'log' ? Math.max(this.extremes.yMax, this.extremes.yMinPositive + 1) : this.extremes.yMax,
           ]);
 
         // Assign the scales to the axes
@@ -109,27 +108,30 @@
           .selectAll('g').
           data(this.data)
           .join('g')
-          .style("fill", (d, i) => this.colors[i])
+          .style("fill", (d, i) => this.colors[i%this.colors.length])
               .selectAll("circle")
-              .data(d => d)
+              .data(d => d.filter(d => {
+                if (Number.isNaN(d[0])) return false;
+                if (Number.isNaN(d[1])) return false;
+                // Filter out negative values if log scales
+                if (this.opts.x.type === 'log' && d[0] <= 0) return false;
+                if (this.opts.y.type === 'log' && d[1] <= 0) return false;
+                return true;
+              }))
               .join("circle")
               .attr("cx", d => this.x(d[0]))
               .attr("cy", d => this.y(d[1]))
               .attr("r", 5.5);
-
-        //
-        const path = d3.line().curve(d3.curveNatural)
-          .x((d, i) => this.x(i))
-          .y(d => this.y(d));
-        this.lines = this.processed.vals.map(i => path(i));
       },
     },
     computed: {
       extremes() {
         const extremes = {
           xMin: Infinity,
+          xMinPositive: Infinity,
           xMax: 0,
           yMin: Infinity,
+          yMinPositive: Infinity,
           yMax: 0
         };
 
@@ -137,45 +139,22 @@
           const xs = series.map(p => p[0]);
           const xMax = Math.max(...xs);
           const xMin = Math.min(...xs);
+          const xMinPositive = Math.min(...xs.filter(x => x > 0));
           if (xMax > extremes.xMax) extremes.xMax = xMax;
           if (xMin < extremes.xMin) extremes.xMin = xMin;
+          if (xMinPositive < extremes.xMinPositive) extremes.xMinPositive = xMinPositive;
+
 
           const ys = series.map(p => p[1]);
           const yMax = Math.max(...ys);
           const yMin = Math.min(...ys);
+          const yMinPositive = Math.min(...ys.filter(y => y > 0));
           if (yMax > extremes.yMax) extremes.yMax = yMax;
           if (yMin < extremes.yMin) extremes.yMin = yMin;
+          if (yMinPositive < extremes.yMinPositive) extremes.yMinPositive = yMinPositive;
         }
 
         return extremes;
-      },
-      processed() {
-        const processed = {};
-
-        if (Array.isArray(this.vals[0])) {
-          processed.vals = this.vals;
-        } else {
-          processed.vals = [this.vals];
-        }
-
-        const extremes = {
-          xMin: 0,
-          xMax: 0,
-          yMin: Infinity,
-          yMax: 0
-        };
-
-        for (const valArr of processed.vals) {
-          if (valArr.length - 1 > extremes.xMax) extremes.xMax = valArr.length - 1;
-
-          const yMax = Math.max(...valArr);
-          const yMin = Math.min(...valArr);
-          if (yMax > extremes.yMax) extremes.yMax = yMax;
-          if (yMin < extremes.yMin) extremes.yMin = yMin;
-        }
-        processed.extremes = extremes;
-
-        return processed;
       },
       opts() {
         const defaultOptions = {
@@ -191,6 +170,15 @@
           }
         };
         return _.merge({}, defaultOptions, this.options);
+      },
+      x() {
+        let x;
+        if (this.opts.y && this.opts.x.type === "log") {
+          x = d3.scaleLog();
+        } else {
+          x = d3.scaleLinear();
+        }
+        return x;
       },
       y() {
         let y;
